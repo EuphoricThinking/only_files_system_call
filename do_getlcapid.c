d1d2468c313006a32aa5493db7b6d8dfce6708de
Agata <amomot@bio-prodict.nl>
2022-05-11 20:41:22 +0200

    Channging to pids

--- do_getlcapid.c
+++ do_getlcapid.c
@@ -2,41 +2,42 @@
 #include "mproc.h"
 #include <errno.h>
 
-int find_depth(mproc* given) {
-	mproc* current = given;
-	int parent = current->mp_parent;
+typedef struct mproc mproc;
+
+int find_depth(mproc current) {
+	int parent = current.mp_parent;
 	int depth = 0;
 
-	while (current->mp_pid != mproc[parent]->mp_pid) {
+	while (current.mp_pid != mproc[parent].mp_pid) {
 		current = mproc[parent];
-		parent = current->mp_parent;
+		parent = current.mp_parent;
 		depth++;
 	}
 
 	return depth;
 }
 
-void shorten(mproc* given, int given_depth, int final_depth) {
+void shorten(mproc given, int given_depth, int final_depth) {
 	while (given_depth > final_depth) {
-		given = mproc[given->mp_parent];
+		given = mproc[given.mp_parent];
 		given_depth--;
 	}
 }
 
 
-pid_t find_lca(mproc* proc1, mproc* proc2) {
-	while (mproc[proc1->mp_parent]->mp_pid != mproc[proc2->mp_parent]->mp_pid) {
-		proc1 = mproc[proc1->mp_parent];
-		proc2 = mproc[proc2->mp_parent];
+pid_t find_lca(mproc proc1, mproc proc2) {
+	while (mproc[proc1.mp_parent].mp_pid != mproc[proc2.mp_parent].mp_pid) {
+		proc1 = mproc[proc1.mp_parent];
+		proc2 = mproc[proc2.mp_parent];
 	}
 
-	return mproc[proc1->mp_parent]->mp_pid;
+	return mproc[proc1.mp_parent].mp_pid;
 }
 
 int do_getlcapid(void) {
 	mproc* proc1 = find_proc(m_in.m1_i1);
 
-	m_in.m1_i3 = -1;
+/*	m_in.m1_i3 = -1;
 	if (!proc1) {
 		return EINVAL;
 	}
@@ -46,12 +47,12 @@ int do_getlcapid(void) {
 		return EINVAL;
 	}
 
-	int depth1 = find_depth(proc1);
+	int depth1 = find_depth(*proc1);
 	if (depth1 == 0) {
 		return ESRCH;
 	}
 
-	int depth2 = find_depth(proc2);
+	int depth2 = find_depth(*proc2);
 	if (depth2 == 0) {
 		return ESCHR;
 	}
@@ -66,7 +67,7 @@ int do_getlcapid(void) {
 		shorten(proc2, depth2, depth1);
 	}
 
-	m_in.m1_i3 = find_lca(proc1, proc2);
+	m_in.m1_i3 = find_lca(*proc1, *proc2); */
 
 	return m_in.m1_i3;
 }
